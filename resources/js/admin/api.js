d422a9bd6119ab4507d51d1dcd8c228b51ec1ebe
officialbektur <officialbektur@gmail.com>
2023-11-12 22:23:31 +0600

    UPDATE

--- resources/js/admin/api.js
+++ resources/js/admin/api.js
@@ -5,30 +5,33 @@ const api = axios.create();
 
 api.interceptors.request.use(config =>  {
 	if (localStorage.getItem('access_token')) {
-		let access_token = localStorage.getItem('access_token');
-		if (access_token !== 'undefined' || access_token.length > 0) {
-			config.headers.authorization = `Bearer ${access_token}`;
+		if (localStorage.getItem('access_token') !== 'undefined' || localStorage.getItem('access_token').length > 0) {
+			config.headers.authorization = `Bearer ${localStorage.getItem('access_token')}`;
 		}
 	}
 	return config;
 }, error => {});
 
-api.interceptors.response.use({}, error => {
+api.interceptors.response.use(response => {
+		return response;
+	}, error => {
 	if ('Token has expired' === error.response.data.message) {
 		return axios.post('/api/admin/auth/refresh', {}, {
 			headers: {
-				'authorization': `Bearer ${localStorage.getItem('access_token')}`
+			'authorization': `Bearer ${localStorage.getItem('access_token')}`
 			}
 		}).then(res => {
 			localStorage.setItem('access_token', res.data.access_token);
 			error.config.headers.authorization = `Bearer ${res.data.access_token}`;
-			return api.request(error.config)
-		})
+			return api.request(error.config);
+		});
 	}
 	if (error.response.status == 401) {
 		localStorage.removeItem('access_token');
-		router.push({name: 'users.login'})
+		router.push({name: 'users.login'});
 	}
+
+	return Promise.reject(error);
 });
 
 export default api
