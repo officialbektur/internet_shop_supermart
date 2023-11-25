839de88090cb05c41e6da1cfbcad6e9265d45c11
officialbektur <officialbektur@gmail.com>
2023-11-25 18:09:25 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,28 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/admin/app.scss',
-                'resources/js/admin/app.js',
-                'resources/sass/project/app.scss',
-                'resources/js/project/app.js',
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
-    },
+	plugins: [
+		laravel({
+			input: [
+				'resources/sass/admin/app.scss',
+				'resources/js/admin/app.js',
+				'resources/sass/project/app.scss',
+				'resources/js/project/app.js',
+			],
+			refresh: true,
+		}),
+		vue({
+			template: {
+				transformAssetUrls: {
+					base: null,
+					includeAbsolute: false,
+				},
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			vue: 'vue/dist/vue.esm-bundler.js',
+		},
+	},
 });
